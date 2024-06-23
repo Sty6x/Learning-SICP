3b2df198b3331ce84748732d13fbfaf713eccb6e
francis <franzdiaz460@gmail.com>
2024-06-23 16:14:01 +0400

    wew

--- main.rkt
+++ main.rkt
@@ -1,6 +1,9 @@
 #lang racket
-
+#| Dont need to use print statement because
+Lisp evaluates and prints automatically  |#
 
 (define (sum x) (+ x x))
-
-(sum 5)
+(+ (sum 5)
+   (* 2 5)
+   )
+(define size 12)
